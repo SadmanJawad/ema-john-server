0da0f749a223ffe7696bf21289061abdce6c9d39
SadmanJawad <jawadnabil977@gmail.com>
2023-05-17 01:09:34 +0600

    use Post to load some products using keys

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixkqk3t.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,6 +43,15 @@ async function run() {
         res.send({totalProducts: result})
     })
 
+    // data loading from client side
+    app.post('/productsByIds', async(req, res) => {
+        const ids = req.body;
+        const objectIds = ids.map(id => new ObjectId(id));
+        const query = { _id: { $in: objectIds } }
+        console.log(ids);
+        const result = await productCollection.find(query).toArray();
+        res.send(result);
+      })
 
 
     // Send a ping to confirm a successful connection
